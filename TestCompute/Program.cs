c0e1a9701a5fcf2d2605d3fb073b445c4ef14944
hiron <hiro.n.rgrk@gmail.com>
2019-09-09 16:06:41 +0900

    Update Program.cs

--- TestCompute/Program.cs
+++ TestCompute/Program.cs
@@ -8,7 +8,7 @@ using Rhino.Compute;
 namespace TestCompute {
     class Program {
         static void Main(string[] args) {
-            ComputeServer.AuthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwIjoiUEtDUyM3IiwiYyI6IkFFU18yNTZfQ0JDIiwiYjY0aXYiOiIwVldHb1ByK3kvWk1SdWZZaFhibXZ3PT0iLCJiNjRjdCI6IlAza2F6SFdpYVJKUlpoOTBETXZLRUxwQVM1S01IUHE4YjlabEZRVHovK3BxK3JKK0pacFlzblBKalduK05RYUdBazF4UytsVi9IZnRHd05XdFNUVE1GdWNDT2ZuNEZKbXZ3UlBBQnNZYW5QaWFOVUdTSSsrTlY2cDF1ZCtVY2ZJS1NRK2hubWdHdkRFN05tbnk0U2RZZytIemhrMmRhMjBrNE1WeDNwUVlJcEFqNWt3cnN1Q2xtVWpnYndKUmJvWlRBWXcvRDJ1WEZ3ZHBkcWhKZk1HYXc9PSIsImlhdCI6MTU2Nzk0MTYyMX0.lOg95YZnMLvif8uYxfpF2qyPT7tfw2J9Pll1WHQRDn0";
+            ComputeServer.AuthToken = "Input your　AuthToken";
 
             var model = new Rhino.FileIO.File3dm();
 
