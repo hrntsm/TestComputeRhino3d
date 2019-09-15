6e7e3c19a28c791b8144bb4f21fa2e2a5088536e
hiron <hiro.n.rgrk@gmail.com>
2019-09-15 20:55:40 +0900

    [add] Circle Intersection
    Comment out the last time Main file and make new Main file which compute circle Intersection and make its svg file

--- TestCompute/Program.cs
+++ TestCompute/Program.cs
@@ -7,35 +7,35 @@ using Rhino.Compute;
 
 namespace TestCompute {
     class Program {
-        static void Main(string[] args) {
-            ComputeServer.AuthToken = "Input your　AuthToken";
+        //static void Main(string[] args) {
+        //    ComputeServer.AuthToken = "Set Your AuthToken";
 
-            var model = new Rhino.FileIO.File3dm();
+        //    var model = new Rhino.FileIO.File3dm();
 
-            // Uses standard Rhino3dmIO methods to create a sphere and add Brep data to model
-            var sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 12);
-            var sphereAsBrep = sphere.ToBrep();
-            model.Objects.AddBrep(sphereAsBrep);
+        //    // Uses standard Rhino3dmIO methods to create a sphere and add Brep data to model
+        //    var sphere = new Rhino.Geometry.Sphere(Rhino.Geometry.Point3d.Origin, 12);
+        //    var sphereAsBrep = sphere.ToBrep();
+        //    model.Objects.AddBrep(sphereAsBrep);
 
-            // the following function calls compute.rhino3d to get access to something not
-            // available in Rhino3dmIO. In this case send Brep to compute and get mesh back.
-            var meshes = MeshCompute.CreateFromBrep(sphereAsBrep);
+        //    // the following function calls compute.rhino3d to get access to something not
+        //    // available in Rhino3dmIO. In this case send Brep to compute and get mesh back.
+        //    var meshes = MeshCompute.CreateFromBrep(sphereAsBrep);
 
-            // Use regular Rhino3dmIO local calls to count the vertices in the mesh
-            Console.WriteLine($"got {meshes.Length} meshes");
-            for (int i = 0; i < meshes.Length; i++) {
-                Console.WriteLine($" {i + 1} mesh has {meshes[i].Vertices.Count} vertices");
+        //    // Use regular Rhino3dmIO local calls to count the vertices in the mesh
+        //    Console.WriteLine($"got {meshes.Length} meshes");
+        //    for (int i = 0; i < meshes.Length; i++) {
+        //        Console.WriteLine($" {i + 1} mesh has {meshes[i].Vertices.Count} vertices");
 
-                //Add meshes data to model
-                model.Objects.AddMesh(meshes[i]);
-            }
+        //        //Add meshes data to model
+        //        model.Objects.AddMesh(meshes[i]);
+        //    }
 
-            // Output Rhino 3dm file
-            var path = System.IO.Directory.GetCurrentDirectory() + "/Outputs/model.3dm";
-            model.Write(path, 5);
+        //    // Output Rhino 3dm file
+        //    var path = System.IO.Directory.GetCurrentDirectory() + "/Outputs/model.3dm";
+        //    model.Write(path, 5);
 
-            Console.WriteLine("press any key to exit");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("press any key to exit");
+        //    Console.ReadKey();
+        //}
     }
 }
